6cd81a547fd7c95975f55aadd2377ed12ecfe33b
Bartosz Jarocki <bartosz.jarocki@smartsoft.io>
2017-01-04 12:11:47 +0100

    Added AppComponent.

--- index.android.js
+++ index.android.js
@@ -1,27 +1,4 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Navigator,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import { AppRegistry } from 'react-native'
+import AppComponent from './app/components/AppComponent'
 
-import { Scene, Router } from 'react-native-router-flux';
-
-import SourcesComponent from './app/components/Sources';
-import NewsComponent from './app/components/News';
-
-export default class NewsReader extends Component {
-  render() {
-    return (
-      <Router>
-        <Scene key="root">
-          <Scene key="sources" component={SourcesComponent} title="Sources" initial={true} />
-          <Scene key="news" component={NewsComponent} title="News" />
-        </Scene>
-      </Router>
-    );
-  }
-}
-
-AppRegistry.registerComponent('NewsReader', () => NewsReader);
+AppRegistry.registerComponent('NewsReader', () => AppComponent)
